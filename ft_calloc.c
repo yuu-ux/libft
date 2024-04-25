c4ef6f928371c77977ccac07f7b95235ba3411a5
Yuga Ehara <yehara@c2r8s10.42tokyo.jp>
2024-04-25 22:03:43 +0900

    create start calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,14 +6,40 @@
 /*   By: yehara <yehara@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 20:54:41 by yehara            #+#    #+#             */
-/*   Updated: 2024/04/25 21:28:16 by yehara           ###   ########.fr       */
+/*   Updated: 2024/04/25 22:03:08 by yehara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*calloc(size_t count, size_t size);
+void *calloc(size_t count, size_t size)
+{
+    //例外処理と0埋めだけ
+    void ft_bzero(void *s, size_t n)
+}
 int main(void)
 {
-	
+	// char *str = "abcde";
+    int dest;
+
+    size_t count = 5;
+    size_t size = 5;
+    dest = (int)calloc(count, size);
+    dest = 1;
+    printf("%d\n", dest);
+    return 0;
 }
+
+
+// 好きな時に失敗する malloc()
+// #if 1
+// void    *malloc(size_t n)
+// {
+//     static int i = 0;
+
+//     if (i++ < 42)
+//         return calloc(n, 1);
+//     errno = ENOMEM;
+//     return (NULL);                                                         
+// }
+// #endif
