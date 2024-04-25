fbe1e98a6e9073e04f8792a4aeb2f9e766df47f6
yuu-ux <yuugaehara@gmail.com>
2024-04-25 18:55:18 +0900

    atoi revision

--- ft_atoi.c
+++ ft_atoi.c
@@ -12,50 +12,51 @@
 
 #include "libft.h"
 
-int	ascii_to_int(const char *str, int flag)
+int ascii_to_int(const char *str, int flag)
 {
-	unsigned int	result;
+    unsigned long result;
 
-	result = 0;
-	while (*str >= '0' && *str <= '9')
-	{
-		result = (result * 10) + (*str - '0');
-		if ((result >= (unsigned int)LONG_MAX / 10) && (result % 10 > 7))
-		{
-			if (flag == 1)
-			{
-				return ((int)LONG_MAX);
-			}
-			else
-			{
-				return ((int)LONG_MIN);
-			}
-		}
-		str++;
-	}
-	return (flag * result);
+    result = 0;
+    while (*str >= '0' && *str <= '9')
+    {
+        result *= 10;
+        result += *str - '0';
+
+        if ((result >= (unsigned long)LONG_MAX / 10) && (result % 10 > 7))
+        {
+            if (flag == 1)
+            {
+                return ((int)LONG_MAX);
+            }
+            else
+            {
+                return ((int)LONG_MIN);
+            }
+        }
+        str++;
+    }
+    return (flag * result);
 }
 
-int	ft_atoi(const char *str)
+int ft_atoi(const char *str)
 {
-	int	flag;
-	int	result;
+    int flag;
+    int result;
 
-	flag = 1;
-	while (*str == ' ' || *str == '\t' || *str == '\n' || *str == '\v'
-		|| *str == '\f' || *str == '\r')
-		str++;
-	if (*str == '-' || *str == '+')
-		if (*str == '-')
-			flag = -1;
-	str++;
-	result = ascii_to_int(str, flag);
-	return (result);
+    flag = 1;
+    while (*str == ' ' || *str == '\t' || *str == '\n' || *str == '\v' || *str == '\f' || *str == '\r')
+        str++;
+    if (*str == '-' || *str == '+')
+        if (*str == '-')
+            flag = -1;
+    str++;
+    result = ascii_to_int(str, flag);
+    return (result);
 }
 
 // int main(void)
 // {
-//     char *str = "9223372036854775807";
+//     char *str = "9223372036854775806";
 //     printf("%d\n", atoi(str));
 //     printf("%d\n", ft_atoi(str));
 //     return (0);
--- ft_calloc.c
+++ ft_calloc.c
@@ -13,5 +13,4 @@
 #include "libft.h"
 int main(void)
 {
-	
 }
