4ab5bfb83ff1dc2a11057cb05cc16198446261b5
yuu-ux <yuugaehara@gmail.com>
2025-02-18 20:11:33 +0900

    :recycle: 定数に置き換え

--- ft_xmalloc.c
+++ ft_xmalloc.c
@@ -8,7 +8,7 @@ void *ft_xmalloc(size_t size)
     if (!tmp)
     {
         ft_printf("malloc error\n");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     ft_memset(tmp, 0, size);
     return (tmp);
