e11e37cfeca8400a424c0004a9b83792f1b5a2ae
yuu-ux <yuugaehara@gmail.com>
2025-04-05 20:53:44 +0900

    :wrench: xmalloc に変更

--- srcs/ft_lstnew.c
+++ srcs/ft_lstnew.c
@@ -16,9 +16,7 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*new_node;
 
-	new_node = (t_list *)malloc(sizeof(t_list));
-	if (new_node == NULL)
-		return (NULL);
+	new_node = (t_list *)ft_xmalloc(sizeof(t_list));
 	new_node->content = content;
 	new_node->next = NULL;
 	return (new_node);
